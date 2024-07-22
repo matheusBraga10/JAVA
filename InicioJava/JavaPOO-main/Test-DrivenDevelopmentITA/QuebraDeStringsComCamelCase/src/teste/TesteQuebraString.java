e3b78280a1168d52a5968e3ef77fc42aa5eebf6f
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2024-07-21 23:49:39 -0300

    CamelCase JAVA TDD

--- InicioJava/JavaPOO-main/Test-DrivenDevelopmentITA/QuebraDeStringsComCamelCase/src/teste/TesteQuebraString.java
+++ InicioJava/JavaPOO-main/Test-DrivenDevelopmentITA/QuebraDeStringsComCamelCase/src/teste/TesteQuebraString.java
@@ -1,56 +1,54 @@
 package teste;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
+import camelCase.CamelCase;
 
 public class TesteQuebraString {
 
-	private QuebraString quebraString;
+	private CamelCase camelCase;
 	
-	
-	/*
-	 * PRIMEIRO TESTE: 
-	 * Teste adicionado: testStringConstrutor()
-	 * Estado do código anterior: Nulo
-	 * Estado do código posterior ao teste: Foi criada a classe QuebraString com seu construtor e metodo printConstrutor, porém todos nulos;
-	 * Foi feito somente uma correção dos pontos que estavam alertando erro no código do teste.
-	 * O teste não rodou (o que já era previsto).
-	 * Como o teste não rodou, vamos para etapa de refatoração do código
-	 * 
-	 * SEGUNDO TESTE:
-	 * Teste adicionado: testSeparaString()
-	 * Estado do código: Foi refatorado para rodar a string de retorno do construtor corretamente, e criado o metodo converterCamelCase() que até o momento retorna nulo;
-	 * O teste não rodou (previsto);
-	 * Segue a parte de refatoração do código para rodar;
-	 * */
-	
-	
-	
-	@Test
-	public void testLeStringConstrutor() {
-		quebraString = new QuebraString("nomeComposto");
-		assertEquals("nomeComposto", quebraString.printConstrutor());
-	}
-
-	@Test
-	public void testSeparaString() {
-		quebraString = new QuebraString("");
-		assertEquals("nome Composto", QuebraString.separarPalavras());
-	}
-	
-	@Test
-	public void testConverterCamelCase() {
-		quebraString = new QuebraString("nomeComposto");
-		assertEquals(new ArrayList<String>(Arrays.asList("nome, composto")), QuebraString.converterCamelCase("nomeComposto"));
 		
+	@Test
+	public void nome() {
+		camelCase = new CamelCase("nome");
+		ArrayList<String> retorno = new ArrayList<String>();
+		retorno.add("nome");
+		assertEquals(Arrays.asList(retorno), Arrays.asList(CamelCase.converterCamelCase()));
 	}
+	
+	@Test
+	public void nomeMaiusculo() {
+		camelCase = new CamelCase("Nome");
+		ArrayList<String> retorno = new ArrayList<String>();
+		retorno.add("nome");
+		assertEquals(Arrays.asList(retorno), Arrays.asList(CamelCase.converterCamelCase()));
+	}
+	
+	@Test
+	public void nomeComposto() {
+		camelCase = new CamelCase("nomeComposto");
+		ArrayList<String> retorno = new ArrayList<String>();
+		retorno.add("nome");
+		retorno.add("composto");
+		 assertEquals(Arrays.asList(retorno), Arrays.asList(CamelCase.converterCamelCase()));
+	}
+	
+	@Test
+	public void CPF() {
+		camelCase = new CamelCase("CPF");
+		ArrayList<String> retorno = new ArrayList<String>();
+		retorno.add("CPF");
+		assertEquals(Arrays.asList(retorno), Arrays.asList(CamelCase.converterCamelCase()));
+	}
+	
+	
 	
 
 }
+	
