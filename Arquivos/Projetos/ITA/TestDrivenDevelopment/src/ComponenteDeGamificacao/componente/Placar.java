127bbccbe26624e19452124b482b02a2ab51b75d
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2024-08-11 21:43:45 -0300

    TDD Java ITA - Concluido
    11/08/24

--- Arquivos/Projetos/ITA/TestDrivenDevelopment/src/ComponenteDeGamificacao/componente/Placar.java
+++ Arquivos/Projetos/ITA/TestDrivenDevelopment/src/ComponenteDeGamificacao/componente/Placar.java
@@ -1,23 +1,47 @@
 package ComponenteDeGamificacao.componente;
 
+import java.util.HashMap;
+
 public class Placar {
 
+	Armazenamento armazenamento;
+
+	private Usuario usuario;
+	private String tipoPonto;
+	private Integer ponto;
 	
+	Usuario nomeArmazenado = ((Placar) armazenamento.armazenaDados.keySet()).getUsuario();
+	String pontoArmazenado = (String) ((HashMap<Object, Object>) armazenamento.armazenaDados.get(getUsuario())).get(getTipoPonto());
+	String tipoPontoArmazenado = (String) (armazenamento.armazenaDados.get(getTipoPonto()));
 	
-	static Armazenamento armazenamento = new Armazenamento();
-	
-	public static Object regitraPontosUsuario(String usuario, Integer ponto, String tipoPonto) {
-		armazenamento.armazenaDados(usuario);
-		return "O usuário " + usuario + " recebeu " + ponto + " pontos do tipo " + tipoPonto;
+	public Object registraPontosUsuario() {
+		armazenamento = new Armazenamento();
+		armazenamento.armazenaDado(usuario, tipoPonto, ponto);
+		return "O usuário " + nomeArmazenado +  " recebeu " + pontoArmazenado + " pontos do tipo " + tipoPontoArmazenado;
 	}
 
 	public String retornaPontosUsuario() {
-		
-		
-		return "O usuário " + armazenamento. +" tem " + armazenamento.recebePonto(ponto, null) + " pontos do tipo " + armazenamento.recebePonto(null, tipoPonto);
+		return "O usuário " + nomeArmazenado +" tem " + pontoArmazenado + " pontos do tipo " + tipoPontoArmazenado;
 	}
 	
 
-	
+	private Usuario getUsuario() {
+		return usuario;
+	}
+	protected void setUsuario(String usuario) {
+		getUsuario();
+	}
+	private String getTipoPonto() {
+		return tipoPonto;
+	}
+	protected void setTipoPonto(String tipoPonto) {
+		getTipoPonto();
+	}
+	private Integer getPonto() {
+		return ponto;
+	}
+	protected void setPonto(Integer ponto) {
+		getPonto();
+	}
 
 }
--- Arquivos/Projetos/ITA/TestDrivenDevelopment/src/ComponenteDeGamificacao/teste/TesteArmazenamento.java
+++ Arquivos/Projetos/ITA/TestDrivenDevelopment/src/ComponenteDeGamificacao/teste/TesteArmazenamento.java
@@ -2,24 +2,90 @@ package ComponenteDeGamificacao.teste;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import ComponenteDeGamificacao.componente.Placar;
+import ComponenteDeGamificacao.componente.Armazenamento;
+import ComponenteDeGamificacao.componente.Usuario;
 
 public class TesteArmazenamento {
-
-	Placar placar;
+	
+	Usuario usuario;
+	Armazenamento armazenamento;
+	HashMap<Object, Object> expected;
+	HashMap<Object, Object> interExpected;
+	
+	@Before
+	public void carregaDados() {
+		armazenamento = new Armazenamento();
+		expected = new HashMap<>();
+		interExpected = new HashMap<>();
+		usuario = new Usuario("Matheus");
+		
+	}
 	
 	@Test
-	public void testArmazenarResultado() {
-		placar = new Placar();
-		assertEquals("O usuário Matheus recebeu 10 pontos do tipo estrela", Placar.regitraPontosUsuario("Matheus", 10, "estrela"));
+	public void testArmazenaDados() {
+		interExpected.put("estrela", 10);
+		expected.put(usuario, interExpected);
+		assertEquals(expected
+				, (armazenamento.armazenaDado(usuario, "estrela", 10)));
 	}
 
 	@Test
-	public void testRecuperar() {
-		placar = new Placar();
-		assertEquals("O usuário Matheus tem 10 pontos do tipo estrela", Placar.retornaPontosUsuario("Matheus"));
+	public void testRecuperaDados() {
+		interExpected.put("estrela", 10);
+		expected.put(usuario, interExpected);
+		armazenamento = new Armazenamento();
+		armazenamento.armazenaDado(usuario, "estrela", 10);
+		assertEquals(expected
+				, (armazenamento.recuperaPontoPorTipo()));
 	}
+
+	@Test
+	public void testNaoRetornaQuandoNaoTemPonto() {
+		armazenamento = new Armazenamento();
+		armazenamento.armazenaDado(usuario, "estrela", 0);
+		assertEquals(null
+				, (armazenamento.recuperaPontoPorTipo()));
+	}
+	
+	@Test
+	public void testRetornaQuandoTemPonto() {
+		interExpected.put("estrela", 10);
+		expected.put(usuario, interExpected);
+		armazenamento = new Armazenamento();
+		armazenamento.armazenaDado(usuario, "estrela", 10);
+		assertEquals(expected
+				, (armazenamento.recuperaPontoPorTipo()));
+	}
+
+	@Test
+	public void testRetornaTodosQuandoTemPonto() {
+		Usuario usuario1;
+		Usuario usuario2;
+		Usuario usuario3;
+		usuario1 = new Usuario("Matheus");
+		usuario2 = new Usuario("Lucas");
+		usuario3 = new Usuario("Max");
+		interExpected.put("estrela", 10);
+		interExpected.put("estrela", 0);
+		interExpected.put("moeda", 50);
+		expected.put(usuario1, interExpected);
+		expected.put(usuario2, interExpected);
+		expected.put(usuario3, interExpected);
+		
+		armazenamento = new Armazenamento();
+		armazenamento.armazenaDado(usuario1, "estrela", 10);
+		armazenamento.armazenaDado(usuario2, "estrela", 0);
+		armazenamento.armazenaDado(usuario3, "moeda", 50);
+		
+		assertEquals(expected
+				, (armazenamento.retornaTodosComPonto()));
+	}
+	
+
 
 }
